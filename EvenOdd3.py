b7631d9e5123dcbe5896e2c05a5d6633fb01b96c
Abhilasha-KV <abhilashakv0313@gmail.com>
2024-07-09 16:10:28 +0530

    added hungry prompt

--- EvenOdd3.py
+++ EvenOdd3.py
@@ -1,11 +1,17 @@
 def your_code():
 
-    number = 7
+    '''number = 7
     reminder = number % 2
 
     if reminder == 1:
         print("its odd")
     else:
-        print("its even")
+        print("its even")'''
+    user = input ("Are you hungry?: ")
+
+    if user=="yes":
+        print("Eat")
+    else:
+        print("Dont eat")
 
 your_code()
